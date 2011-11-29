f67486faedd48e695ac36e10e0f50c91a43bd73f
Mathieu Lecarme <mathieu@garambrogne.net>
2011-11-29 23:07:46 +0100

    Gravatar.

--- test.js
+++ test.js
@@ -1,17 +1,23 @@
 var Book = require('./lib/book').Book,
+    Gravatar = require('./lib/gravatar').Gravatar,
     WhitePages = require('./lib/whitepages').WhitePages;
 
 var wp = new WhitePages();
-var book = new Book([{
-        objectclass: [ "top" ],
-        cn: "Robert Dupond",
-        mail: "robert@dupond.com",
-        givenname: "Robert",
-        sn: "bob",
-        ou: "dupondinc"
-      }]);
+var g = new Gravatar('mathieu@garambrogne.net')
+g.jpeg(function(jpeg) {
+    var book = new Book([{
+            objectclass: ["person", "top" ],
+            cn: "Robert Dupond",
+            mail: "robert@dupond.com",
+            givenname: "Robert",
+            sn: "bob",
+            ou: "dupondinc",
+            jpegphoto: jpeg
+          }]);
 
-wp.read(book);
-wp.listen(1389, '127.0.0.1', function(){
-    console.log("Withe pages at %s", wp.ldap.url);
-})
+    wp.read(book);
+    wp.listen(1389, '127.0.0.1', function(){
+        console.log("Withe pages at %s", wp.ldap.url);
+    })
+
+});
