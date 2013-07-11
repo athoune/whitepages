08a110989fc1ede1d5fde95b76c04f2d85e4dae5
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-12 00:02:40 +0200

    Book as json.

--- test.js
+++ test.js
@@ -1,29 +1,13 @@
 var Gravatar = require('./lib/gravatar').Gravatar,
     wp = require('./lib/whitepages'),
     WhitePages = wp.WhitePages,
-    Book = wp.Book;
+    JsonBook = wp.JsonBook;
 
 var wp = new WhitePages();
 var g = new Gravatar('mathieu@garambrogne.net');
 g.jpeg(function(avatar) {
-    var book = new Book([{
-            objectclass: ["person", "top" ],
-            cn: ["Robert Dupond junior", "Blob"],
-            mail: "robert@dupond.com",
-            givenname: "Robert",
-            sn: "Dupont",
-            o: "dupondinc",
-            ou: "sales",
-            'jpegPhoto;binary': avatar,
-            c: "france",
-            labeledURI: "http://github.com",
-            nickname: "Bob",
-            xmozillanickname: "Bob le mozillien",
-            info: "more infos",
-            uid: "rdupond",
-            description: "Description",
-            displayName: "RoBeRt"
-          }]);
+    var book = new JsonBook('test.json');
+    console.log(book.data);
     wp.read(book);
     wp.listen(1389, '127.0.0.1', function(){
         console.log("White pages at %s", wp.ldap.url);
