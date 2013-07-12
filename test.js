d3103ad5d20bbc4a1d579905a7c2701192d30d2c
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-12 18:46:21 +0200

    Fix: dump.

--- test.js
+++ test.js
@@ -7,7 +7,6 @@ var wp = new WhitePages('o=whitepages');
 var g = new Gravatar('mathieu@garambrogne.net');
 g.jpeg(function(avatar) {
     var book = new JsonBook('test.json');
-    console.log(book.data);
     wp.read(book);
     wp.listen(1389, '127.0.0.1', function(){
         console.log("White pages at %s", wp.ldap.url);
