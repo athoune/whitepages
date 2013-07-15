5c4a5dad167420a3dc727e60ddb0ca2cae4bf178
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-15 11:59:39 +0200

    Some authentication.

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 var Gravatar = require('./lib/gravatar').Gravatar,
     white = require('./lib/whitepages.js'),
+    crypto = require('crypto'),
     WhitePages = white.WhitePages,
     JsonBook = white.JsonBook;
 
+
 var wp = new WhitePages('o=whitepages');
+var h = crypto.createHmac('sha256', 'wp');
+h.update('foo');
+wp.hash_key = 'wp';
+wp.password = h.digest('hex');
 var g = new Gravatar('mathieu@garambrogne.net');
 g.jpeg(function(avatar) {
     var book = new JsonBook('test.json');
